d30449d1065806f3d239207816daf1f455205a75
Evgenii Rodkin <rodkin1987@gmail.com>
2019-01-01 15:13:43 +0200

    Created a database connection class

---
+++ classes/Connect/ConnectDB.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jack
+ * Date: 01.01.19
+ * Time: 15:05
+ */
+
+namespace classes\Connect;
+
+
+abstract class ConnectDB {
+	public static function connect() : mixed {
+		try {
+			$pdo = new \PDO('pgsql:host=localhost;dbname=accounting','jack','Alxi9mik');
+			return $pdo;
+		} catch (\PDOException $e) {
+			return false;
+		}
+	}
+}
