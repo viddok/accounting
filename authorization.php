198e57feffc50c664925fd7bfa23ac56a802ca3f
Evgenii Rodkin <rodkin1987@gmail.com>
2019-01-06 23:39:14 +0200

    del forwarding when user login

--- authorization.php
+++ authorization.php
@@ -8,11 +8,6 @@ $users = Users::create_collection();
 
 $error = '';
 
-if ( isset( $_SESSION['current_user'] ) ) {
-	header( "location: index.php" );
-	exit();
-}
-
 if ( isset($_POST['user_id']) ) {
 	if ( is_numeric( $_POST['user_id'] ) ) {
 		foreach ($users->getCollection() as $user) {
@@ -42,39 +37,37 @@ if ( isset($_POST['user_id']) ) {
 	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Авторизация пользователя</title>
-	<style>
-		html, body {
-			height: 100%;
-			margin: 0;
-			padding: 0;
-			overflow: hidden;
-		}
-		body {
-			overflow: auto; /* добавить полосу прокрутки */
-			background-color: cornsilk;
-			display: flex;
-			align-items: center;
-			justify-content: center
-		}
-		select {
-			min-width: 200px;
-		}
-	</style>
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="style/authorization.css">
 </head>
 <body>
-	<?php echo $error; ?>
-	<form method="post">
-		<label>
-			Выберите пользователя: <br />
-			<select name='user_id'>
-				<?php
-				foreach ($users->getCollection() as $user) {
-					echo "<option value='$user->id'>$user->name</option>";
-				}
-				?>
-			</select>
-		</label>
-		<input type="submit" value="Авторизоваться">
-	</form>
+<div class="wrap">
+    <div class="top-menu">
+        <?php
+        if ( isset( $_SESSION['current_user'] ) ) {
+	        echo 'Привет, <a href="authorization.php" title="Сменить пользователя.">' . $_SESSION['current_user']['name'] . '</a>';
+        } else {
+            echo '<a href="authorization.php" title="Выберите пользователя.">Авторизуйтесь</a>';
+        }
+        ?>
+    </div>
+
+    <div class="content">
+        <?php echo $error; ?>
+        <form method="post">
+            <label>
+                Выберите пользователя: <br />
+                <select name='user_id'>
+                    <?php
+                    foreach ($users->getCollection() as $user) {
+                        echo "<option value='$user->id'>$user->name</option>";
+                    }
+                    ?>
+                </select>
+            </label>
+            <input type="submit" value="Авторизоваться">
+        </form>
+    </div>
+</div>
 </body>
 </html>
