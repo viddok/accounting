647752ad3fd0ede0c2b688d49cd143c6efdd656d
Evgenii Rodkin <rodkin1987@gmail.com>
2019-01-07 00:50:51 +0200

    Создал функию проверки роли Админа

--- func.php
+++ func.php
@@ -7,16 +7,27 @@
  */
 
 /** Автозагрузка классов */
-function __autoload ($classname) {
-	$classname = str_replace('\\', '/', $classname);
-	require_once (__DIR__ . "/$classname.php");
+function __autoload( $classname ) {
+	$classname = str_replace( '\\', '/', $classname );
+	require_once( __DIR__ . "/$classname.php" );
 }
 
 /** Переадресация пользователя */
-function forwarding_auth () {
+function forwarding_auth() {
 	if ( ! isset( $_SESSION['current_user'] ) ) {
 		header( "location: authorization.php" );
 		exit();
 	}
+
+	return false;
+}
+
+function is_admin(): bool {
+	if ( isset( $_SESSION['current_user'] ) ) {
+		if ( 'admin' === $_SESSION['current_user']['role'] ) {
+			return true;
+		}
+	}
+
 	return false;
 }
