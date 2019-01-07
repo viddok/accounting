d1aa7fef014e97979da0ea4ea81797d8cb2ab644
Evgenii Rodkin <rodkin1987@gmail.com>
2019-01-07 10:26:24 +0200

    Добавил PhpDoc для перебора массива объектов User

--- authorization.php
+++ authorization.php
@@ -2,18 +2,20 @@
 session_start();
 
 require_once 'func.php';
+
 use classes\Users\Users;
 
 $users = Users::create_collection();
 
 $error = '';
 
-if ( isset($_POST['user_id']) ) {
+if ( isset( $_POST['user_id'] ) ) {
 	if ( is_numeric( $_POST['user_id'] ) ) {
-		foreach ($users->getCollection() as $user) {
+		foreach ( $users->getCollection() as $user ) {
+			/** @var $user \classes\Users\User\User */
 			if ( $_POST['user_id'] == $user->getId() ) {
 				$current_user = array(
-					'id' => $user->getId(),
+					'id'   => $user->getId(),
 					'name' => $user->getName(),
 					'role' => $user->getRole(),
 				);
@@ -32,37 +34,38 @@ if ( isset($_POST['user_id']) ) {
 <!doctype html>
 <html lang="ru">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport"
-	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Авторизация пользователя</title>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Авторизация пользователя</title>
     <link rel="stylesheet" href="style/style.css">
     <link rel="stylesheet" href="style/authorization.css">
 </head>
 <body>
 <div class="wrap">
     <div class="top-menu">
-        <?php
-        if ( isset( $_SESSION['current_user'] ) ) {
-	        echo 'Привет, <a href="authorization.php" title="Сменить пользователя.">' . $_SESSION['current_user']['name'] . '</a>';
-        } else {
-            echo '<a href="authorization.php" title="Выберите пользователя.">Авторизуйтесь</a>';
-        }
-        ?>
+		<?php
+		if ( isset( $_SESSION['current_user'] ) ) {
+			echo 'Привет, <a href="authorization.php" title="Сменить пользователя.">' . $_SESSION['current_user']['name'] . '</a>';
+		} else {
+			echo '<a href="authorization.php" title="Выберите пользователя.">Авторизуйтесь</a>';
+		}
+		?>
     </div>
 
     <div class="content">
-        <?php echo $error; ?>
+		<?php echo $error; ?>
         <form method="post">
             <label>
-                Выберите пользователя: <br />
+                Выберите пользователя: <br/>
                 <select name='user_id'>
-                    <?php
-                    foreach ($users->getCollection() as $user) {
-                        echo "<option value='{$user->getId()}'>{$user->getName()}</option>";
-                    }
-                    ?>
+					<?php
+					foreach ( $users->getCollection() as $user ) {
+						/** @var $user \classes\Users\User\User */
+						echo "<option value='{$user->getId()}'>{$user->getName()}</option>";
+					}
+					?>
                 </select>
             </label>
             <input type="submit" value="Авторизоваться">
