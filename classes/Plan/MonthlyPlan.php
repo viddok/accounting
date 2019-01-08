eb187505d5478dcb88acf3225f633eadcc846d6d
Evgenii Rodkin <rodkin1987@gmail.com>
2019-01-08 21:32:29 +0200

    Реализовал конструктор

--- classes/Plan/MonthlyPlan.php
+++ classes/Plan/MonthlyPlan.php
@@ -12,10 +12,27 @@ use classes\Connect\ConnectDB;
 
 
 class MonthlyPlan {
+
 	/** @var  array int */
 	protected $categories;
+	protected $date;
+
+	/** @var  \PDO */
+	private $connect;
 
 	public function __construct() { // Конструктор
+		$this->date = date('m-Y');
+
+		$this->connect = ConnectDB::connect();
+
+		try {
+			$query = 'SELECT * FROM plan WHERE date = :date';
+			$tmp = $this->connect->prepare($query);
+			$tmp->execute(['date' => $this->date]);
+			$this->categories = $tmp->fetchAll(\PDO::FETCH_ASSOC);
+		} catch( \PDOException $e ) {
+			echo 'Ошибка выполнения запроса ' . $e->getMessage();
+		}
 	}
 
 	public function add_category(){ // Добавляет новую категорию
