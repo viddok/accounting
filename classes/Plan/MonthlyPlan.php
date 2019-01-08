ebb3db23c81c6dad3fec19d4d4c4abcdd182b845
Evgenii Rodkin <rodkin1987@gmail.com>
2019-01-08 21:13:42 +0200

    Создал класс планирования расходовна месяц по категориям

---
+++ classes/Plan/MonthlyPlan.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jack
+ * Date: 08.01.19
+ * Time: 21:00
+ */
+
+namespace classes\Plan;
+
+use classes\Connect\ConnectDB;
+
+
+class MonthlyPlan {
+	/** @var  array int */
+	protected $categories;
+
+	public function __construct() { // Конструктор
+	}
+
+	public function add_category(){ // Добавляет новую категорию
+
+	}
+
+	public function delete_category(){ // Удаляет выбранную категорию
+
+	}
+
+	public function set_sum(){ // Назначает сумму указанной категории
+
+	}
+
+	public function copy_previous_plan(){ // Копирует план с предыдущего месяца
+
+	}
+}
