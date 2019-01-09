96a1cead090ed2fc1af9dcb2a85a3c00b3e73353
Evgenii Rodkin <rodkin1987@gmail.com>
2019-01-09 06:19:14 +0200

    Реализовал метод добавления категории

--- classes/Plan/MonthlyPlan.php
+++ classes/Plan/MonthlyPlan.php
@@ -13,7 +13,6 @@ use classes\Connect\ConnectDB;
 
 class MonthlyPlan {
 
-	/** @var  array int */
 	protected $categories;
 	protected $date;
 
@@ -21,22 +20,62 @@ class MonthlyPlan {
 	private $connect;
 
 	public function __construct() { // Конструктор
-		$this->date = date('m-Y');
+		$this->date = date( 'm-Y' );
 
 		$this->connect = ConnectDB::connect();
 
 		try {
 			$query = 'SELECT * FROM plan WHERE date = :date';
-			$tmp = $this->connect->prepare($query);
-			$tmp->execute(['date' => $this->date]);
-			$this->categories = $tmp->fetchAll(\PDO::FETCH_ASSOC);
+			$tmp = $this->connect->prepare( $query );
+			$tmp->execute( [':date' => $this->date] );
+			$this->categories = $tmp->fetchAll( \PDO::FETCH_ASSOC );
 		} catch( \PDOException $e ) {
 			echo 'Ошибка выполнения запроса ' . $e->getMessage();
 		}
 	}
 
-	public function add_category(){ // Добавляет новую категорию
+	public function add_category( string $title, float $sum ): bool { // Добавляет новую категорию
+		$id   = null;
+		$copy = false;
 
+		foreach ( $this->categories as $category ) { // Проверка на добавление дубликата
+			if ( $category['category'] === $title && $category['sum'] == $sum && $category['date'] === $this->date ) {
+				$copy = true;
+			}
+		}
+
+		if ( ! $copy ) {
+			$query = 'INSERT INTO plan ( category, sum, date ) VALUES ( :category, :sum, :date ) RETURNING id';// Добавление категории в базу данных и получаем id
+			try {
+				$tmp  = $this->connect->prepare( $query );
+				$args = array(
+					':category' => htmlspecialchars( $title ),
+					':sum'      => $sum,
+					':date'     => $this->date,
+				);
+				if ( ! $tmp->execute( $args ) ) {
+					return false;
+				}
+				$arr = $tmp->fetchAll( \PDO::FETCH_ASSOC );
+				$id  = $arr[0]['id'];
+			} catch ( \PDOException $e ) {
+				echo 'Ошибка выполнения запроса ' . $e->getMessage();
+
+				return false;
+			}
+
+			$number                      = count( $this->categories );
+			$this->categories[ $number ] = array(
+				'id'       => $id,
+				'category' => $title,
+				'sum'      => $sum,
+				'date'     => $this->date,
+			);
+
+			return true;
+		}
+
+		return false;
 	}
 
 	public function delete_category(){ // Удаляет выбранную категорию
