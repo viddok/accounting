c426e900c947d2325a547d080ec6f94567f8907e
Evgenii Rodkin <rodkin1987@gmail.com>
2019-01-09 22:28:00 +0200

    Создал шаблон меню

---
+++ templates/main-menu.php
@@ -0,0 +1,7 @@
+<nav class="nav top-nav">
+	<ul>
+		<li><a href="accounts.php">Мои счета</a></li>
+		<li><a href="monthly-plan.php">Планирование</a></li>
+		<li><a href="">Пункт 3</a></li>
+	</ul>
+</nav>
