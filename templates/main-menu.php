6d3e7e54349e75e1640951f2841d165a9459f85c
Evgenii Rodkin <rodkin1987@gmail.com>
2019-01-11 09:16:28 +0200

    Обновил меню

--- templates/main-menu.php
+++ templates/main-menu.php
@@ -3,5 +3,6 @@
 		<li><a href="accounts.php">Мои счета</a></li>
 		<li><a href="monthly-plan.php">Планирование</a></li>
 		<li><a href="expenses.php">Расходы</a></li>
+        <li><a href="purchase-log.php">История</a></li>
 	</ul>
 </nav>
